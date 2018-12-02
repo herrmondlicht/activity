6797d63146753999428e8b502eb5d13d2acd3651
Guilherme Moreira <guilhermegms.silva@gmail.com>
2018-12-02 11:51:42 -0200

    feat(mainPage): render
    Adds Menu on rendering

--- front/src/components/Main/MainPage.js
+++ front/src/components/Main/MainPage.js
@@ -2,16 +2,43 @@ import React from 'react'
 import styles from './styles/MainPage.css'
 import QuestionOnePage from '../QuestionOnePage';
 import CustomAppBar from '../CustomAppBar';
-export const createMainPage = (React) => {
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Menu from '../BottomMenu/Menu';
+
+const ROUTES = [
+  {
+    name: 'Question One',
+    to: '/question1'
+  },
+  {
+    name: 'Question Two',
+    to: '/question2'
+  },
+  {
+    name: 'Question Three',
+    to: '/question3'
+  },
+  {
+    name: 'Reel Question',
+    to: '/reel-question'
+  }
+]
+
+export const createMainPage = (React, { routes = ROUTES } = {}) => {
 
   class MainPage extends React.Component {
     render() {
       return (
         <div className={styles['main-page']}>
           <CustomAppBar title={'Question One '} />
-          <div className={styles['page-container']}>
-            <QuestionOnePage />
-          </div>
+          <Router>
+            <div className={styles['page-container']}>
+              <div className={styles['page-container-content']}>
+                <Route exact path='/question1' component={QuestionOnePage} />
+              </div>
+              <Menu routes={routes} />
+            </div>
+          </Router>
         </div>
       )
     }
