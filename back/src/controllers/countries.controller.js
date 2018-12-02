df19f6406630696e99adfa2e56f865ddca6a948d
Guilherme Moreira <guilhermegms.silva@gmail.com>
2018-12-02 15:50:26 -0200

    feat(countries.controller): getCountryWithNamesInArray
    Adds a function to search multiple countries at once

--- back/src/controllers/countries.controller.js
+++ back/src/controllers/countries.controller.js
@@ -1,23 +1,41 @@
 import fetch from 'isomorphic-fetch'
 
-export default ({ fetchCountriesAPI = _fetchCountriesAPI } = {}) => (
-  {
-    getCountryByName: (req, res) => {
-      const { params: { name }, query } = req;
-      const paramsToCountryAPI = `name/${name}`
-      fetchCountriesAPI({ params: paramsToCountryAPI, query })
-        .then(country => res.status(200).json(country))
-    },
-    getCountryWithNamesInArray: () => { },
-    getCountriesList: () => {
-
+export default (
+  { fetchCountriesAPI = _fetchCountriesAPI,
+    returnResponse = _returnResponse,
+    createPromiseNameArray = _createPromiseNameArray,
+    removeErrorsFromArray = _removeErrorsFromArray } = {}) => (
+    {
+      getCountryByName: async (req, res) => {
+        const { params: { name }, query } = req;
+        const paramsToCountryAPI = `name/${name}`
+        const responseFromAPI = await fetchCountriesAPI({ params: paramsToCountryAPI, query })
+        return returnResponse(responseFromAPI, res)
+      },
+      getCountryWithNamesInArray: (req, res) => {
+        const { search, searchFullText } = req.query
+        const castedSearch = JSON.parse(search)
+        const promiseArray = createPromiseNameArray(castedSearch.nameArray, searchFullText)
+        Promise.all(promiseArray)
+          .then(response => returnResponse(removeErrorsFromArray(response.flat()), res))
+      },
+      getCountriesList: async (req, res) => {
+        const params = '/all'
+        const responseFromAPI = await fetchCountriesAPI({ params })
+        return returnResponse(responseFromAPI, res)
+      }
     }
-  }
-)
+  )
 
-/*
-I guess it's not necessary to write unit tests for the function below. This has to be an integration test.
- */
+const _returnResponse = (value, res) => res.status(200).json(value)
+
+const _createPromiseNameArray = (nameArray, searchFullText) =>
+  nameArray.map(name => {
+    return _fetchCountriesAPI({ params: `name/${name}`, query: { fullText: searchFullText } })
+  })
+
+const _removeErrorsFromArray = (array) => array.filter(item => !item.status)
+
 const _fetchCountriesAPI = ({
   countriesAPIEndpoint = process.env.COUNTRIES_ENDPOINT,
   params,
--- back/src/routes/countries.routes.js
+++ back/src/routes/countries.routes.js
@@ -1,6 +1,7 @@
 import getCountryController from '../controllers/countries.controller'
 import express from 'express'
 export default ({ router = express.Router(), controller = getCountryController() } = {}) => {
+  router.get('/countries/multiple', controller.getCountryWithNamesInArray)
   router.get('/countries/:name', controller.getCountryByName)
   return router
 }
