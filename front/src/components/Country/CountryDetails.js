bf2dbcf091fca646a66e13526c701dbdcc90602e
Guilherme Moreira <guilhermegms.silva@gmail.com>
2018-12-02 16:01:23 -0200

    reafactor(countryDetails): style
    Get a listy-style instead of a details page per se

--- front/src/components/Country/CountryDetails.js
+++ front/src/components/Country/CountryDetails.js
@@ -4,36 +4,26 @@ import styles from './styles/CountryDetails.css'
 import CountryDetailsField from './CountryDetailsField';
 
 export const createCountryDetails = (React) => {
-
-
-  class CountryDetails extends React.Component {
-
-    static propTypes = {
-      data: propTypes.object
-    }
-
-
-
-    render() {
-      const { data } = this.props
-      return (
-        <div className={styles['country-details']}>
-          <div className={styles['country-details__header']}>
-            <img src={data.flag} className={styles['country-details__header__flag']} alt="flag" />
-          </div>
-          <div className={styles['country-details__body']}>
-            <CountryDetailsField value={data.name} title={'Name'} />
-            <CountryDetailsField value={data.capital} title={'Capital'} />
-            <CountryDetailsField value={data.region} title={'Region'} />
-            <CountryDetailsField value={data.population} title={'Population'} />
-            <CountryDetailsField value={data.demonym} title={'Demonym'} />
-            <CountryDetailsField value={data.currencies[0] && data.currencies[0].name} title={'Main Currency'} />
-          </div>
+  
+  const CountryDetails = ({ data }) =>
+    <div className={styles['country-details']}>
+      <div className={styles['country-details__flag-container']}>
+        <img src={data.flag} className={styles['country-details__flag-container__flag']} alt="flag" />
+      </div>
+      <div className={styles['country-details__body']}>
+        <div className={styles['country-details__body__block']}>
+          <CountryDetailsField value={data.name} title={'Name'} />
+          <CountryDetailsField value={data.capital} title={'Capital'} />
+          <CountryDetailsField value={data.region} title={'Region'} />
         </div>
-      )
-    }
+        <div className={styles['country-details__body__block']}>
+          <CountryDetailsField value={data.population} title={'Population'} />
+          <CountryDetailsField value={data.demonym} title={'Demonym'} />
+          <CountryDetailsField value={data.currencies && data.currencies[0] && data.currencies[0].name} title={'Main Currency'} />
+        </div>
 
-  }
+      </div>
+    </div>
 
   return CountryDetails
 
