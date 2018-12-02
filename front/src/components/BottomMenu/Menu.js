c93198e6f846b009ce76d5f392183267f49efdbd
Guilherme Moreira <guilhermegms.silva@gmail.com>
2018-12-02 11:51:02 -0200

    feat(menu): component
    Creates component to help navigation throughout application

---
+++ front/src/components/BottomMenu/Menu.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import styles from './styles/Menu.css'
+
+export const createMenu = (React) => {
+
+
+  class Menu extends React.Component {
+
+    static propTypes = {
+      routes: propTypes.array.isRequired
+    }
+    
+    render() {
+      const { routes } = this.props
+      return (
+        <div className={styles['menu-container']}>
+          {
+            routes.map(route => (
+              <div className={styles['menu-container__link']}>
+                <Link
+                style={{textDecoration:'none'}} 
+                to={route.to}>{route.name}</Link>
+              </div>
+            ))
+          }
+        </div>
+      )
+    }
+
+  }
+
+  return Menu
+
+}
+
+export default createMenu(React)
